cdbf7713d58a211a27b91f1617b5253e427c7bcd
박준홍 <63082558+Junhong0209@users.noreply.github.com>
2021-09-25 13:16:54 +0900

    Revert "."
    This reverts commit c60bee4741f48feab1701067397ff66fe3de8b17.

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import { Route } from "react-router-dom";
 import './App.scss'
 
 import FirstPage from './Components/FirstPage/FirstPage';
+import SignIn from "./Components/SignIn/SignIn";
 
 function App() {
   return (
     <div className="App">
       <Route path='/' component={FirstPage} exact />
+      <Route path='/Login' component={SignIn} />
     </div>
   );
 }
