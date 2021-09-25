7816f71f46608121e033c596cf86ae78faeacd54
박준홍 <63082558+Junhong0209@users.noreply.github.com>
2021-09-25 13:17:26 +0900

    Revert "Revert "Fix: change font name""
    This reverts commit 21cb17b574852c579f140198d8bbb471bffd76ce.

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.scss'
 
 import FirstPage from './Components/FirstPage/FirstPage';
 import SignIn from "./Components/SignIn/SignIn";
+import SignUp from "./Components/SignUp/SignUp";
 
 function App() {
   return (
     <div className="App">
       <Route path='/' component={FirstPage} exact />
       <Route path='/Login' component={SignIn} />
+      <Route path='/Signup' component={SignUp} />
     </div>
   );
 }
