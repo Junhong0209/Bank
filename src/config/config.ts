4bfeabe674507451c27dcaa24a7ebe0f9236df18
박준홍 <63082558+Junhong0209@users.noreply.github.com>
2021-11-18 15:35:01 +0900

    Update: Api Header 값 변경

--- src/config/config.ts
+++ src/config/config.ts
@@ -2,13 +2,7 @@ export const baseURL: string = 'http://34.64.165.105:8080';
 
 export const header: object = {
   headers: {
-    'Content-Type': 'application/x-www-form-urlencoded'
-  }
-}
-
-export const PostHeader: object = {
-  headers: {
-    'Content-Type': 'application/x-www-form-urlencoded',
-    'Authorization': sessionStorage.getItem('token')
+    'Access-Control-Allow-Origin': '*',
+    'Authorization': `Bearer ${sessionStorage.getItem('access-token')}`
   }
 }
